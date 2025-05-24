cc4e251487d016a12da96fffc041a3cd24f0fee7
Sanzhar Turabaev <167844412+Hilrein@users.noreply.github.com>
2025-05-25 00:30:04 +0500

    edit for render

--- sapartime/sapartime/steppe/steppe/settings.py
+++ sapartime/sapartime/steppe/steppe/settings.py
@@ -25,6 +25,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+
+
+
+
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'steppe.urls'
@@ -98,6 +106,20 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+
+
+
+# для рендера на render
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+
+
+
+
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
